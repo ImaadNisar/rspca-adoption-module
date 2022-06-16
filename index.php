75744bb9bdc007f307b3ef60eccd64621d934ec5
Imaad Nisar <62900453+ImaadNisar@users.noreply.github.com>
2022-06-16 07:58:33 +0100

    Add files via upload
    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include("templates/header.php"); ?>
+
+<div class="search-bar-container">
+    <form action="search.php" method="POST">
+        <input type="text" name="search" placeholder="Search" id="search-bar">
+        <input type="submit" name="submit-search" value="" id="search-btn">
+    </form>
+</div>
+
+<div class="animals-container">
+    
+</div>
+
+
+<?php include("templates/footer.php"); ?>
+
